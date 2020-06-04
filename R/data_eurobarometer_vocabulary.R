b91025270efe0fdcb8454bfce4e4d1c9ad1a3bfc
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-06-04 22:21:09 +0200

    eurobarometer_vocabulary

---
+++ R/data_eurobarometer_vocabulary.R
@@ -0,0 +1,14 @@
+#' GESIS Eurobarometer Variable Names
+#'
+#' A controlled vocabulary for creating variable names.
+#'
+#' @format A data_frame of three variables:
+#' \describe{
+#'   \item{canonical_name}{The name of the variable in the data file.}
+#'   \item{harmonized_name}{The original SPSS numeric values, if applicable}
+#'   \item{var_group}{The original SPSS character value, if applicable}
+#' }
+#' @seealso gesis_corpus_create gesis_vocabulary_create
+"eurobarometer_vocabulary"
+
+
