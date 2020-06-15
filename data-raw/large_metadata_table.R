0672dbbe3c31e7fcc45b1efb67a94a8bfabae141
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-06-15 18:09:35 +0200

    intendation

--- data-raw/large_metadata_table.R
+++ data-raw/large_metadata_table.R
@@ -2,7 +2,8 @@ metadata_table <- readRDS(file.path("data-raw",
                             "eb_metadata_database_large.rds"))
 
 small_table <- metadata_table %>%
-  filter ( grepl( "trust|age_exact|uniqid|occupation", var_name_suggested ) )
+  filter ( grepl( "trust|age_exact|uniqid|occupation",
+                  var_name_suggested ) )
 
 
 id_table <- metadata_table %>%
