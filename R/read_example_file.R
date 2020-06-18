c0ab089878461c3e0dfa55c52df41f812f6885e5
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-06-18 19:33:04 +0200

    example file reader for example data pipelines

---
+++ R/read_example_file.R
@@ -0,0 +1,33 @@
+#' @title Read A eurobarometer Package Example File
+#'
+#' This function is created to make panel data creation examples
+#' without relying on importing large SPSS files.
+#'
+#' @param sample_file_name Any of \code{ZA7576_sample},
+#' \code{ZA7562_sample} or \code{ZA7489_sample}.
+#' @importFrom utils data
+#' @examples
+#' read_example_file ( sample_file_name = 'ZA7576_sample')
+#' @export
+
+
+read_example_file <- function( sample_file_name = 'ZA7576_sample' ) {
+
+  sample_file_name <- substr( sample_file_name,1,6)
+  if ( ! sample_file_name %in% c('ZA7576',
+                                 'ZA7562',
+                                 'ZA7489') ) {
+    stop ( sample_file_name , "_sample is not among the package sample data files.")
+  }
+
+  if (sample_file_name == 'ZA7576' ) {
+    data ('ZA7576_sample')
+    return(ZA7576_sample)
+  } else if (sample_file_name == 'ZA7562') {
+    data ('ZA7562_sample')
+    return(ZA7562_sample)
+  } else if (sample_file_name == 'ZA7489') {
+    data ('ZA7489_sample')
+    return(ZA7489_sample)
+  }
+}
