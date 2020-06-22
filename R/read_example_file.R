7b77ca24e782ed9cc806588b1e0945fe425ec9c7
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-06-22 22:45:48 +0200

    example files are changed

--- R/read_example_file.R
+++ R/read_example_file.R
@@ -4,7 +4,7 @@
 #' without relying on importing large SPSS files.
 #'
 #' @param sample_file_name Any of \code{ZA7576_sample},
-#' \code{ZA7562_sample} or \code{ZA7489_sample}.
+#' \code{ZA6863_sample} or \code{ZA5913_sample}.
 #' @importFrom utils data
 #' @examples
 #' read_example_file ( sample_file_name = 'ZA7576_sample' )
@@ -12,14 +12,14 @@
 
 read_example_file <- function( sample_file_name = 'ZA7576_sample' ) {
 
-  . <- ZA7576_sample <- ZA7562_sample <- ZA7489_sample <- NULL
+  . <- ZA7576_sample <- ZA6863_sample <- ZA5913_sample <- NULL
   class_orig <- n_categories <- panel_id <- val_label_orig <- NULL
   val_numeric_orig <- var_name_orig <- NULL
 
   sample_file_name <- substr( sample_file_name,1,6)
   if ( ! sample_file_name %in% c('ZA7576',
-                                 'ZA7562',
-                                 'ZA7489') ) {
+                                 'ZA6863',
+                                 'ZA5913') ) {
     stop ( sample_file_name ,
            "_sample is not among the package sample data files.")
   }
@@ -27,11 +27,11 @@ read_example_file <- function( sample_file_name = 'ZA7576_sample' ) {
   if (sample_file_name == 'ZA7576' ) {
     data ('ZA7576_sample', envir = environment())
     return(ZA7576_sample)
-  } else if (sample_file_name == 'ZA7562') {
-    data ('ZA7562_sample', envir = environment())
-    return(ZA7562_sample)
-  } else if (sample_file_name == 'ZA7489') {
-    data ('ZA7489_sample', envir = environment())
-    return(ZA7489_sample)
+  } else if (sample_file_name == 'ZA6863') {
+    data ('ZA6863_sample', envir = environment())
+    return(ZA6863_sample)
+  } else if (sample_file_name == 'ZA5913') {
+    data ('ZA5913_sample', envir = environment())
+    return(ZA5913_sample)
   }
 }
