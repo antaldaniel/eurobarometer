85f5b8a312f84a7a4441e4ff1af8eac9187444fc
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-06-26 21:35:40 +0200

    Don't run with labelled

--- R/harmonize_to_numeric.R
+++ R/harmonize_to_numeric.R
@@ -6,12 +6,14 @@
 #' @param harmonized_labelled_var A harmonized labelled var created
 #' by \code{\link{harmonize_value_labels}}
 #' @examples
+#' \dontrun{
 #' v <- labelled::labelled(c(3,4,4,3,8, 9),
 #'              c(YES = 3, NO = 4, wrong_label = 8, refused = 9)
 #'              )
 #' input_labelled_var = harmonize_value_labels(v)
 #'
 #' harmonize_to_numeric(input_labelled_var)
+#' }
 #' @export
 
 harmonize_to_numeric <- function( harmonized_labelled_var ) {
