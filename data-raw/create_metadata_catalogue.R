5f71480994eec3f7d3f007eabf9a578ab9bf7c16
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-06-28 14:08:51 +0200

    Corrections, many of them....

--- data-raw/create_metadata_catalogue.R
+++ data-raw/create_metadata_catalogue.R
@@ -8,19 +8,19 @@ gesis_files <- file.path(gesis_dir,
 spss_files <- dir(gesis_dir)[grepl(".sav", dir(gesis_dir))]
 gesis_file <- spss_files[5]
 
-gesis_file_list <- split(gesis_files, ceiling(seq_along(gesis_files)/3))
-paste ( gsub( paste0(gesis_dir, "\\/"), "", gesis_file_list[[1]]))
-
+gesis_file_list <- split(spss_files, ceiling(seq_along(spss_files)/3))
 n_batches <- length(gesis_file_list)
 
+message ( length(spss_files), " SPSS files will be read in ", n_batches, " batches." )
 for (i in 1:n_batches) {
 
   start_time <- Sys.time()
   message ( i, "/", n_batches ,  " Reading ",
-            paste ( gsub( paste0(gesis_dir, "\\/"), "",
-                          gesis_file_list[[1]]), collapse = "; "))
+            paste (gesis_file_list[[i]], collapse = "; "))
 
-  tmp_surveys <- read_surveys( gesis_file_list[[1]], .f = "read_spss_survey" )
+  tmp_surveys <- read_surveys( gesis_file_list[[i]],
+                               .f = "read_spss_survey",
+                               file_path = gesis_dir)
 
   message ( "Read batch ", i,  "/", n_batches )
 
@@ -30,11 +30,16 @@ for (i in 1:n_batches) {
     tmp_metadata <- gesis_metadata_create(tmp_surveys)
     metadata_database <- dplyr::bind_rows(metadata_database,
                                           tmp_metadata)
+
+    n_files <- length(unique ( metadata_database$filename))
+    n_vars <- length(unique ( metadata_database$var_name_suggested))
+
+
+    message ( "Current vars: ", nrow(metadata_database), " (new: ",
+              nrow(tmp_metadata),
+              "; unique: ",
+              n_vars, ") from ", n_files, " SPSS files." )
   }
-
-  metadata_database$filename <-  gsub( paste0(gesis_dir, "\\/"), "", metadata_database$filename)
-
-  message ( "Added batch ", i,  "/", n_batches )
 
   saveRDS(metadata_database,
           file = file.path("data-raw", "eb_metadata_database_20200628.rds"))
@@ -44,12 +49,15 @@ for (i in 1:n_batches) {
   message ( "Ellapsed time: " , round(end_time - start_time,0), " sec")
 }
 
-write.csv( metadata_database,
-           file = file.path("data-raw", "eb_metadata_database_20200628.csv"),
+write.csv( metadata_database %>%
+             mutate_all( as.character),
+           file = file.path("data-raw",
+                            "eb_metadata_database_20200628.csv"),
            row.names=FALSE )
 
 saveRDS(metadata_database,
-        file = file.path("data-raw", "eb_metadata_database_20200628.rds"))
+        file = file.path("data-raw",
+                         "eb_metadata_database_20200628.rds"))
 
 source(file.path('data-raw', 'create_binary_metadata_table.R'))
 source(file.path('data-raw', 'create_binary_metadata_table.R'))
