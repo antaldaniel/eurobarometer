89c4e2c17c1e54ab7bddbd361078b6085674c47d
Daniel Antal <antaldaniel@users.noreply.github.com>
2020-07-17 18:08:49 +0200

    recasting methods

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,15 @@
+#' @inheritParams retroharmonize::as_factor
+#' @export
+as_factor <- retroharmonize::as_factor
+
+#' @inheritParams retroharmonize::as_numeric
+#' @export
+as_numeric <- retroharmonize::as_numeric
+
+#' @inheritParams retroharmonize::as_character
+#' @export
+as_character <- retroharmonize::as_character
+
 #' Suggest Conversion
 #'
 #' @param metadata A metadata file created by metadata_create within
